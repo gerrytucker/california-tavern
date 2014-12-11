cc5dc9a85b7c0251282f100a5bebda09c9c07cd7
gerrytucker <gerrytucker@gerrytucker.co.uk>
2014-12-11 23:57:08 +0000

    Auto-generated commit 11/12/2014 11:57:03 PM

--- header.php
+++ header.php
@@ -9,7 +9,14 @@
 
 		<div class="row">
 			<div class="col-sm-6 col-md-6 col-lg-6">
-				<h1><?php bloginfo('name'); ?></h1>
+				<div class="box-row">
+					<h1><?php bloginfo('name'); ?></h1>
+				</div>
 			</div>
-			<div class="col-sm-6 col-md-6 col-lg-6"></div>
+			<div class="col-sm-6 col-md-6 col-lg-6">
+				<div class="box-row">
+					<h1><?php bloginfo('name'); ?></h1>
+				</div>
+			</div>
 		</div>
+		
