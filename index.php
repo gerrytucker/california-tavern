8a658224825b6c844f3e23ba2cdc0be5277a6892
Gerry Tucker <gerrytucker@gerrytucker.co.uk>
2015-01-04 21:11:22 +0000

    Auto-generated commit 04/01/2015 9:11:17 PM

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php get_header(); ?>
 
-  <div id="intro">
+  <div id="intro" class="grey darken-4">
     <div class="container">
       <div class="row">
-        <div class="col s12 m12 l12">
+        <div class="col s12 m12 l12 red-text text-darken-3">
           <h1>California<br>Tavern</h1>
         </div>
       </div>
