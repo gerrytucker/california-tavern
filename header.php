62024e490b7af75b8979bb2435846391bf484ec5
Gerry Tucker <gerrytucker@gerrytucker.co.uk>
2015-01-04 17:23:57 +0000

    Auto-generated commit 04/01/2015 5:23:14 PM

--- header.php
+++ header.php
@@ -1,7 +1,12 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-  <title><?php wp_title('|', true, 'right'); ?></title>
-  <?php wp_head(); ?>
-</head>
-<body>
+<!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7"><![endif]-->
+<!--[if IE 7]><html class="no-js lt-ie9 lt-ie8"><![endif]-->
+<!--[if IE 8]><html class="no-js lt-ie9"><![endif]-->
+<!--[if gt IE 8]><!--><html class="no-js" lang="en"><!--<![endif]-->
+	<head>
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title><?php wp_title('|', true, 'right'); ?></title>
+		<?php wp_head(); ?>
+	</head>
+	<body>
